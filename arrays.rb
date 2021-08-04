122998cd2c70d87afd5d05a1d4b2dda4dd622799
saeedghorbanim <animation10733@yahoo.com>
2021-08-03 19:16:18 -0500

    final version

---
+++ arrays.rb
@@ -0,0 +1,24 @@
+a = ["mat", "saeed", "ashley", "ryan", "hugo", "jelly"]
+b = [3,4,5,6,8,3]
+
+puts a.at(0)
+
+puts a.delete("saeed")
+
+puts a.reverse
+
+puts a.length
+
+puts a.sort
+
+puts b.shuffle
+
+puts b.slice(1..4)
+
+puts a
+
+puts b.join
+
+puts b.insert(1, 7)
+
+puts a.values_at(2..4)
